245a497acb8e41c3b1f59cb6d343c2dfa375880b
jtagaca <jtagaca@csub.edu>
2021-06-06 22:02:40 -0700

    Updates

---
+++ precache-manifest.0121e3c1cf5c9f15ef36c66088a3a295.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc13662af382d0efd44d1d47564cc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e51159efc19b1a02215",
+    "url": "/static/css/main.1550c801.chunk.css"
+  },
+  {
+    "revision": "c0506b00113d8d70f542",
+    "url": "/static/js/2.656ea443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.656ea443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e51159efc19b1a02215",
+    "url": "/static/js/main.4b8b7778.chunk.js"
+  },
+  {
+    "revision": "fea57e1c3e9d35159c27",
+    "url": "/static/js/runtime-main.4fc57d6b.js"
+  },
+  {
+    "revision": "30b488906796e9140234fb1febbea960",
+    "url": "/static/media/ITCAvantGardeStd-Bold.30b48890.woff"
+  },
+  {
+    "revision": "8d750edc41957820b8d2031de8c8cf23",
+    "url": "/static/media/ITCAvantGardeStd-Bold.8d750edc.woff2"
+  },
+  {
+    "revision": "a612a1fbee7869bc11783f614ab932a5",
+    "url": "/static/media/ITCAvantGardeStd-Bold.a612a1fb.ttf"
+  },
+  {
+    "revision": "ac3b176875e84eec152966c024e8922d",
+    "url": "/static/media/ITCAvantGardeStd-Bold.ac3b1768.eot"
+  },
+  {
+    "revision": "aaba0c03c1af56be698dec9d764c049e",
+    "url": "/static/media/loading.aaba0c03.svg"
+  }
+]);
